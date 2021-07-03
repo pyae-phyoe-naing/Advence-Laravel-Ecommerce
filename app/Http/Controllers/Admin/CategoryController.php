055a6a4199e620d2a7054d11f23a114e35f7d39d
fazza7777 <aungbarlaay7777@gmail.com>
2021-07-04 00:46:17 +0630

    create category process

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
+use Image;
 use App\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -10,33 +10,96 @@ use Illuminate\Support\Facades\Session;
 
 class CategoryController extends Controller
 {
-    public function categories(){
-        Session::put('page','categories');
+    public function categories()
+    {
+        Session::put('page', 'categories');
         $categories = Category::get();
-        return view('admin.categories.categories',compact('categories'));
+        return view('admin.categories.categories', compact('categories'));
     }
-    public function updateCategoryStatus(Request $request){
-        if($request->ajax()){
+    public function updateCategoryStatus(Request $request)
+    {
+        if ($request->ajax()) {
             $data = $request->all();
-            if($data['status'] === 'Active'){
+            if ($data['status'] === 'Active') {
                 $status = 0;
-            }else{
+            } else {
                 $status = 1;
             }
-            Category::where('id',$data['category_id'])->update(['status'=>$status]);
+            Category::where('id', $data['category_id'])->update(['status' => $status]);
             return response()->json([
                 'status' => $status,
-                'category_id'=>$data['category_id']
+                'category_id' => $data['category_id']
             ]);
         }
     }
-    public function addEditCategory(Request $request,$id=null){
-        if(empty($id)){
-            $title ='Add Category';
-        }else{
+    public function addEditCategory(Request $request, $id = null)
+    {
+        if (empty($id)) {
+            if ($request->isMethod('POST')) {
+                $request->validate([
+                    'category_name' => 'required|regex:/^[\pL\s\-]+$/u',
+                    'section_id' => 'required',
+                    'parent_id' => 'required',
+                    'category_image' => 'required',
+                    'url' => 'required',
+                ],[
+                    'category_name.required' => 'Name is required',
+                    'category_name.regex' => 'Name is valid',
+                    'section_id.required' => 'Section is required',
+                    'parent_id.required' => 'Parent category is required',
+                    'url.required' => 'Category URL is required',
+                    'category_image.required' => 'Image is required',
+                ]);
+                $data = $request->all();
+                $category = new Category();
+                // Upload Image
+                if ($request->hasFile('category_image')) {
+                    $file = $request->file('category_image');
+                    if ($file->isValid()) {
+                        ## get image extension
+                        $extension = $file->getClientOriginalExtension();
+                        ## generate new image name
+                        $imgName = time() . uniqid() . '.' . $extension;
+                        $imgPath = 'images/category_images/' . $imgName;
+                        Image::make($file)->save($imgPath);
+                        // save database
+                        $category->category_image = $imgName;
+
+                    }
+                }
+                if(empty($request->description)){
+                    $data['description'] = '';
+                }
+                if(empty($request->category_discount)){
+                    $data['category_discount'] = '';
+                }
+                if(empty($request->meta_title)){
+                    $data['meta_title'] = '';
+                }
+                if(empty($request->meta_description)){
+                    $data['meta_description'] = '';
+                }
+                if(empty($request->meta_keywords)){
+                    $data['meta_keywords'] = '';
+                }
+                $category->parent_id = $data['parent_id'];
+                $category->section_id = $data['section_id'];
+                $category->category_name = $data['category_name'];
+                $category->category_discount = $data['category_discount'];
+                $category->description = $data['description'];
+                $category->url = $data['url'];
+                $category->meta_title = $data['meta_title'];
+                $category->meta_description = $data['meta_description'];
+                $category->meta_keywords = $data['meta_keywords'];
+                $category->status = 1;
+                $category->save();
+                return redirect('/admin/categories')->with('success','category add success!');
+            } else {
+                $getSection = Section::get();
+            }
+        } else {
             $title = 'EDit Category';
         }
-        $getSection = Section::get();
-        return view('admin.categories.add_edit_category',compact('title','getSection'));
+        return view('admin.categories.add_edit_category', compact('getSection'));
     }
 }
--- resources/views/admin/categories/add_edit_category.blade.php
+++ resources/views/admin/categories/add_edit_category.blade.php
@@ -22,102 +22,133 @@
         <!-- Main content -->
         <section class="content">
             <div class="container-fluid">
-                <!-- SELECT2 EXAMPLE -->
-                <div class="card card-default">
-                    <div class="card-header">
-                        <h3 class="card-title">Add Category</h3>
+                <form action="{{ url('/admin/add-edit-category') }}" method="POST" enctype="multipart/form-data"
+                    name="categoryForm" id="CategoryForm">
+                    @csrf
+                    <div class="card card-default">
+                        <div class="card-header">
+                            <h3 class="card-title">Add Category</h3>
 
-                        <div class="card-tools">
-                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                                <i class="fas fa-minus"></i>
-                            </button>
-                            <button type="button" class="btn btn-tool" data-card-widget="remove">
-                                <i class="fas fa-times"></i>
-                            </button>
-                        </div>
-                    </div>
-                    <!-- /.card-header -->
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="cat_name">Category Name</label>
-                                    <input type="text" class="form-control" id="cat_name" placeholder="Enter name">
-                                </div>
-                                <div class="form-group">
-                                    <label>Select Category Level</label>
-                                    <select name="parent_id" class="form-control select2" style="width: 100%;">
-                                        <option value="0">Main Category</option>
-                                    </select>
-                                </div>
-
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                                <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                    <i class="fas fa-times"></i>
+                                </button>
                             </div>
-                            <!-- /.col -->
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label>Select Session</label>
-                                    <select name="section_id" class="form-control select2" style="width: 100%;">
-                                        <option disabled selected="selected">-- select --</option>
-                                        @foreach ($getSection as $section)
-                                            <option value="{{ $section->id }}">{{ $section->name }}</option>
-                                        @endforeach
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="category_name">Category Name</label>
+                                        <input type="text" name="category_name" class="form-control
+                                            @error('category_name') is-invalid  @enderror" id="category_name"
+                                            placeholder="Enter name">
+                                        @error('category_name')
+                                            <small class="text text-danger"><strong>{{ $message }}</strong></small>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Select Category Level</label>
+                                        <select name="parent_id" id="parent_id"
+                                            class="form-control select2 @error('parent_id') is-invalid  @enderror"
+                                            style="width: 100%;">
+                                            <option value="0">Main Category</option>
+                                        </select>
+                                        @error('parent_id')
+                                            <small class="text text-danger"><strong>{{ $message }}</strong></small>
+                                        @enderror
+                                    </div>
 
-                                    </select>
                                 </div>
-                                <div class="form-group">
-                                    <label for="cat_image">Category Image</label>
-                                    <div class="input-group">
-                                        <div class="custom-file">
-                                            <input type="file" name="category_image" class="custom-file-input" id="cat_image">
-                                            <label class="custom-file-label" for="cat_image">Choose file</label>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>Select Session</label>
+                                        <select name="section_id" id="section_id"
+                                            class="form-control select2 @error('section_id') is-invalid  @enderror"
+                                            style="width: 100%;">
+                                            <option disabled selected="selected">-- select --</option>
+                                            @foreach ($getSection as $section)
+                                                <option value="{{ $section->id }}">{{ $section->name }}</option>
+                                            @endforeach
+
+                                        </select>
+                                        @error('section_id')
+                                            <small class="text text-danger"><strong>{{ $message }}</strong></small>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="cat_image">Category Image</label>
+                                        <div class="input-group">
+                                            <div class="custom-file">
+                                                <input type="file" name="category_image"
+                                                    class="custom-file-input @error('category_image') is-invalid  @enderror"
+                                                    id="category_image">
+                                                <label class="custom-file-label" for="category_image">Choose file</label>
+                                            </div>
+                                            <div class="input-group-append">
+                                                <span class="input-group-text">Upload</span>
+                                            </div>
                                         </div>
-                                        <div class="input-group-append">
-                                            <span class="input-group-text">Upload</span>
-                                        </div>
+                                        @error('category_image')
+                                            <small class="text text-danger"><strong>{{ $message }}</strong></small>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12 col-md-6 ">
+                                    <div class="form-group">
+                                        <label for="cat_discount">Category Discount</label>
+                                        <input type="text" name="category_discount" class="form-control"
+                                            id="category_discount" placeholder="Enter Discount">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="description">Category Description</label>
+                                        <textarea name="description" id="description" class="form-control"
+                                            placeholder="Enter..." rows="3"></textarea>
+
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="form-group">
+                                        <label for='url'>Category Url</label>
+                                        <input type="text" name="url"
+                                            class="form-control @error('url') is-invalid  @enderror" id="url" placeholder="Enter Url">
+                                        @error('url')
+                                            <small class="text text-danger"><strong>{{ $message }}</strong></small>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="meta_title">Meta Title</label>
+                                        <textarea name="meta_title" id="meta_title" placeholder="Enter..."
+                                            class="form-control" rows="3"></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-sm-6">
+                                    <div class="form-group">
+                                        <label for="meta_description">Meta Description</label>
+                                        <textarea name="meta_description" id="meta_description" class="form-control"
+                                            rows="3" placeholder="Enter..."></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-sm-6">
+                                    <div class="form-group">
+                                        <label for="meta_keyword">Meta Keywords</label>
+                                        <textarea name="meta_keywords" id="meta_keywords" placeholder="Enter..."
+                                            class="form-control" rows="3"></textarea>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-12 col-sm-6">
-                                <div class="form-group">
-                                    <label for="cat_discount">Category Discount</label>
-                                    <input type="text" name="category_discount" class="form-control" id="cat_discount" placeholder="Enter Discount">
-                                </div>
-                                <div class="form-group">
-                                    <label for="cat_description">Category Description</label>
-                                    <textarea name="category_description" class="form-control" placeholder="Enter..." rows="3"></textarea>
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="meta_description">Meta Description</label>
-                                    <textarea name="meta_description" class="form-control" rows="3" placeholder="Enter..."></textarea>
-                                </div>
-                            </div>
-                            <div class="col-12 col-sm-6">
-                                <div class="form-group">
-                                    <label for='cat_url'>Category Url</label>
-                                    <input type="text" name="category_url" class="form-control" id="cat_url" placeholder="Enter Url">
-                                </div>
-                                <div class="form-group">
-                                    <label for="meta_title">Meta Title</label>
-                                    <textarea name="meta_title" placeholder="Enter..." class="form-control" rows="3"></textarea>
-                                </div>
-                                <div class="form-group">
-                                    <label for="meta_keyword">Meta Keyword</label>
-                                    <textarea name="meta_keyword" placeholder="Enter..." class="form-control" rows="3"></textarea>
-                                </div>
-
-                            </div>
-
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-primary">Submit</button>
                         </div>
-                        <!-- /.row -->
                     </div>
-                    <!-- /.card-body -->
-                    <div class="card-footer">
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </div>
-                </div>
+                </form>
         </section>
     </div>
 @endsection
